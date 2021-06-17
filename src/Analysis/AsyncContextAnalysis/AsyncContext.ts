7e6417294be92b0ca969a438824c1dccb7195018
Soulike <soulike@qq.com>
2021-06-17 13:06:27 +0800

    完善使用 async_hooks 的上下文追踪

--- src/Analysis/AsyncContextAnalysis/AsyncContext.ts
+++ src/Analysis/AsyncContextAnalysis/AsyncContext.ts
@@ -19,6 +19,7 @@ class AsyncContext extends Analysis
 
     private asyncContextChanged: boolean;
     private lastAsyncId: number;
+    private lastTriggerAsyncId: number;
     private asyncIdToFunctionCall: Map<number, CallbackFunction>;
 
     constructor(sandbox: Sandbox)
@@ -26,10 +27,14 @@ class AsyncContext extends Analysis
         super(sandbox);
         this.asyncContextChanged = false;
         this.lastAsyncId = -1;
-        this.asyncIdToFunctionCall = new Map([[1, CallbackFunction.GLOBAL]]);
+        this.lastTriggerAsyncId = -1;
+        this.asyncIdToFunctionCall = new Map([
+            [CallbackFunction.UNKNOWN_ASYNC_ID, CallbackFunction.UNKNOWN],
+            [CallbackFunction.GLOBAL_ASYNC_ID, CallbackFunction.GLOBAL],
+        ]);
 
         async_hooks.createHook({
-            init(asyncId: number, type: string, triggerAsyncId: number, resource: object) {},
+            init: this.asyncHookInit,
             before: this.asyncHookBefore,
         }).enable();
 
@@ -42,38 +47,55 @@ class AsyncContext extends Analysis
         {
             if (this.asyncContextChanged)
             {
-                const asyncId = async_hooks.executionAsyncId();
-                const triggerAsyncId = async_hooks.triggerAsyncId();
+                const asyncId = this.lastAsyncId;
+                const triggerAsyncId = this.lastTriggerAsyncId;
 
                 const sandbox = this.getSandbox();
                 const sourceCodeInfo = getSourceCodeInfoFromIid(iid, sandbox);
 
-                assert.ok(triggerAsyncId !== 0);    // TODO: 解决 await 问题
-                assert.ok(this.lastAsyncId === asyncId);
+                assert.ok(asyncId !== 0);
 
-                let triggerAsyncFunction = this.asyncIdToFunctionCall.get(triggerAsyncId);
-                // for Promises
-                triggerAsyncFunction = triggerAsyncFunction === undefined ? CallbackFunction.GLOBAL : triggerAsyncFunction;
+                let triggerAsyncFunction: CallbackFunction | null | undefined = this.asyncIdToFunctionCall.get(triggerAsyncId);
+                assert.ok(triggerAsyncFunction !== undefined);
 
+                while (triggerAsyncFunction.func === null
+                && triggerAsyncFunction.asyncId !== CallbackFunction.GLOBAL_ASYNC_ID
+                && triggerAsyncFunction.asyncId !== CallbackFunction.UNKNOWN_ASYNC_ID)
+                {
+                    triggerAsyncFunction = triggerAsyncFunction.asyncScope;    // won't be null, ensured by triggerAsyncFunction.asyncId !== ...
+                    assert.ok(triggerAsyncFunction !== undefined && triggerAsyncFunction !== null);
+                }
 
-                const functionCall = new CallbackFunction(f, asyncId, triggerAsyncFunction, sourceCodeInfo);
-                this.asyncIdToFunctionCall.set(asyncId, functionCall);
-                CallbackFunctionContext.setCurrentCallbackFunction(functionCall);
+                const placeholderAsyncFunction: CallbackFunction | null | undefined = this.asyncIdToFunctionCall.get(asyncId);
+                assert.ok(placeholderAsyncFunction !== undefined);
+
+                const asyncFunction = new CallbackFunction(f, asyncId, placeholderAsyncFunction.type, triggerAsyncFunction, sourceCodeInfo);
+                this.asyncIdToFunctionCall.set(asyncId, asyncFunction);
+                CallbackFunctionContext.setCurrentCallbackFunction(asyncFunction);
 
                 this.asyncContextChanged = false;
                 this.lastAsyncId = -1;
+                this.lastTriggerAsyncId = -1;
             }
         };
     }
 
     // must be an arrow function to fix `this`
+    private asyncHookInit = (asyncId: number, type: string, triggerAsyncId: number, resource: object) =>
+    {
+        const triggerAsyncFunction = this.asyncIdToFunctionCall.get(triggerAsyncId);
+        assert.ok(triggerAsyncFunction !== undefined);
+        const placeholderAsyncFunction = new CallbackFunction(null, asyncId, type, triggerAsyncFunction, null);
+        this.asyncIdToFunctionCall.set(asyncId, placeholderAsyncFunction); // should be overwritten by functionEnter() if there is a function call
+    };
+
+    // must be an arrow function to fix `this`
     private asyncHookBefore = (asyncId: number) =>
     {
-        if (asyncId !== 0)
-        {
-            this.asyncContextChanged = true;
-            this.lastAsyncId = asyncId;
-        }
+        this.asyncContextChanged = true;
+        assert.ok(asyncId === async_hooks.executionAsyncId());
+        this.lastAsyncId = asyncId;
+        this.lastTriggerAsyncId = async_hooks.triggerAsyncId();
     };
 }
 
--- src/Analysis/Class/CallbackFunction.ts
+++ src/Analysis/Class/CallbackFunction.ts
@@ -4,17 +4,23 @@ import SourceCodeInfo from './SourceCodeInfo';
 
 class CallbackFunction
 {
-    public static readonly GLOBAL = new CallbackFunction(null, 1, null, null);
+    public static readonly UNKNOWN_ASYNC_ID = 0;
+    public static readonly UNKNOWN = new CallbackFunction(null, CallbackFunction.UNKNOWN_ASYNC_ID, 'UNKNOWN', null, null);
+
+    public static readonly GLOBAL_ASYNC_ID = 1;
+    public static readonly GLOBAL = new CallbackFunction(null, CallbackFunction.GLOBAL_ASYNC_ID, 'GLOBAL', null, null);
 
     public readonly func: Function | null;
     public readonly asyncId: number;
+    public readonly type: string;
     public asyncScope: CallbackFunction | null;    // null for global
     public readonly registerCodeInfo: SourceCodeInfo | null;
 
-    constructor(func: Function | null, asyncId: number, asyncScope: CallbackFunction | null, registerCodeInfo: SourceCodeInfo | null)
+    constructor(func: Function | null, asyncId: number, type: string, asyncScope: CallbackFunction | null, registerCodeInfo: SourceCodeInfo | null)
     {
         this.func = func;
         this.asyncId = asyncId;
+        this.type = type;
         this.asyncScope = asyncScope; // 被创建时所在的 scope
         this.registerCodeInfo = registerCodeInfo;   // 本 callback 是被什么地方的代码注册执行的
     }
@@ -22,11 +28,11 @@ class CallbackFunction
     /** Whether asyncScope chain is a sub chain of 'this.asyncScope'. e.g. a->b->c is in async scope a->b */
     isInAsyncScope(asyncScope: CallbackFunction): boolean
     {
-        if (asyncScope === CallbackFunction.GLOBAL || this === asyncScope)
+        if (asyncScope === CallbackFunction.GLOBAL || asyncScope === CallbackFunction.UNKNOWN || this === asyncScope)
         {
             return true;
         }
-        if (this === CallbackFunction.GLOBAL)
+        if (this === CallbackFunction.GLOBAL || this === CallbackFunction.UNKNOWN)
         {
             return false;
         }
