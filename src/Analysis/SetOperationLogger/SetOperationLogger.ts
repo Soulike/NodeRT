48629e26b07f96a0bffaf4ebe9a7ad16c4d1fc54
Soulike <soulike@qq.com>
2021-08-19 10:32:43 +0800

    改进了 Set 的建模

--- src/Analysis/SetOperationLogger/SetOperationLogger.ts
+++ src/Analysis/SetOperationLogger/SetOperationLogger.ts
@@ -1,12 +1,17 @@
 // DO NOT INSTRUMENT
 
 import {Analysis, Hooks, Sandbox} from '../../Type/nodeprof';
-import {strict as assert} from 'assert';
 import {ObjectLogStore} from '../../LogStore/ObjectLogStore';
+import {isObject} from 'lodash';
+import {getSourceCodeInfoFromIid, isBufferLike} from '../../Util';
+import {BufferLogStore} from '../../LogStore/BufferLogStore';
 
 export class SetOperationLogger extends Analysis
 {
     public invokeFun: Hooks['invokeFun'] | undefined;
+
+    private static readonly readMethods: Set<Function> = new Set([Set.prototype.forEach, Set.prototype.has]);
+    private static readonly writeMethods: Set<Function> = new Set([Set.prototype.add, Set.prototype.delete, Set.prototype.clear]);
 
     constructor(sandbox: Sandbox)
     {
@@ -17,22 +22,44 @@ export class SetOperationLogger extends Analysis
 
     protected override registerHooks()
     {
-        this.invokeFun = (iid, f, base, _args, result, isConstructor) =>
-        {
-            const readMethods: Function[] = [Set.prototype.forEach, Set.prototype.has];
-            const writeMethods: Function[] = [Set.prototype.add, Set.prototype.delete, Set.prototype.clear];
-            if (f === Set && isConstructor)
-            {
-                assert.ok(result instanceof Set);
-                ObjectLogStore.appendObjectOperation(result, 'write', this.getSandbox(), iid);
+        this.invokeFun = (iid, f, base, args, result) =>
+        {   
+            if (f === Set
+                || f === Set.prototype.entries
+                || f === Set.prototype.keys
+                || f === Set.prototype.values
+                || f === Set.prototype[Symbol.iterator])
+            {   
+                for (const arg of args)
+                {
+                    if (isObject(arg))
+                    {
+                        ObjectLogStore.appendObjectOperation(arg, 'read', this.getSandbox(), iid);
+                        if (isBufferLike(arg))
+                        {
+                            BufferLogStore.appendBufferOperation(arg, 'read',
+                                getSourceCodeInfoFromIid(iid, this.getSandbox()));
+                        }
+                    }
+                }
+
+                if (isObject(result))
+                {
+                    ObjectLogStore.appendObjectOperation(result, 'write', this.getSandbox(), iid);
+                    if (isBufferLike(result))
+                    {
+                        BufferLogStore.appendBufferOperation(result, 'write',
+                            getSourceCodeInfoFromIid(iid, this.getSandbox()));
+                    }
+                }
             }
             else if (base instanceof Set)
             {
-                if (readMethods.includes(f))
+                if (SetOperationLogger.readMethods.has(f))
                 {
                     ObjectLogStore.appendObjectOperation(base, 'read', this.getSandbox(), iid);
                 }
-                else if (writeMethods.includes(f))
+                else if (SetOperationLogger.writeMethods.has(f))
                 {
                     ObjectLogStore.appendObjectOperation(base, 'write', this.getSandbox(), iid);
                 }
