0552ea61072b6f714357515a5e063476776b3577
Soulike <soulike@qq.com>
2021-08-19 11:04:44 +0800

    优化 Array 建模代码

--- src/Analysis/ArrayOperationLogger/ArrayOperationLogger.ts
+++ src/Analysis/ArrayOperationLogger/ArrayOperationLogger.ts
@@ -2,7 +2,8 @@
 
 import {Analysis, Hooks, Sandbox} from '../../Type/nodeprof';
 import {strict as assert} from 'assert';
-import {ObjectLogStore} from '../../LogStore/ObjectLogStore';
+import {logObjectArgsAsReadOperation, logObjectBaseAsReadOperation, logObjectBaseAsWriteOperation, logObjectResultAsWriteOperation} from '../../Util';
+import {isObject} from 'lodash';
 
 /**Does not support spread expression now*/
 export class ArrayOperationLogger extends Analysis
@@ -19,65 +20,55 @@ export class ArrayOperationLogger extends Analysis
 
     protected override registerHooks()
     {
-        const sandbox = this.getSandbox();
-
         this.invokeFun = (iid, f, base, args, result) =>
         {
-            if (f === Array || f === Array.of)
+            if (f === Array
+                || f === Array.of
+                || f === Array.from
+                || f === Array.prototype[Symbol.iterator]
+                || f === Array.prototype.values
+                || f === Array.prototype.keys
+                || f === Array.prototype.entries)
             {
-                assert.ok(Array.isArray(result));
-                ObjectLogStore.appendObjectOperation(result, 'write', sandbox, iid);
-            }
-            else if (f === Array.from)
-            {
-                const iterable = args[0] as Parameters<typeof Array.from>[0];
-                ObjectLogStore.appendObjectOperation(iterable, 'read', sandbox, iid);
-                assert.ok(Array.isArray(result));
-                ObjectLogStore.appendObjectOperation(result, 'write', sandbox, iid);
+                logObjectArgsAsReadOperation(args, this.getSandbox(), iid);
+                logObjectResultAsWriteOperation(result, this.getSandbox(), iid);
             }
             else if (f === Array.prototype.concat)
             {
-                const arg = args[0] as Parameters<typeof Array.prototype.concat>[0];
-                if (Array.isArray(arg))
-                {
-                    ObjectLogStore.appendObjectOperation(arg, 'read', sandbox, iid);
-                }
-
-                assert.ok(Array.isArray(base));
-                ObjectLogStore.appendObjectOperation(base, 'read', sandbox, iid);
-
-                assert.ok(Array.isArray(result));
-                ObjectLogStore.appendObjectOperation(result, 'write', sandbox, iid);
+                logObjectArgsAsReadOperation(args, this.getSandbox(), iid);
+                logObjectResultAsWriteOperation(result, this.getSandbox(), iid);
+                assert.ok(isObject(base));
+                logObjectBaseAsReadOperation(base, this.getSandbox(), iid);
             }
-            else if (f === Array.prototype.copyWithin)
+            else if (f === Array.prototype.copyWithin
+                || f === Array.prototype.fill
+                || f === Array.prototype.pop
+                || f === Array.prototype.push
+                || f === Array.prototype.shift
+                || f === Array.prototype.reverse
+                || f === Array.prototype.sort
+                || f === Array.prototype.unshift)
             {
-                assert.ok(Array.isArray(base));
-                ObjectLogStore.appendObjectOperation(base, 'read', sandbox, iid);
-                ObjectLogStore.appendObjectOperation(base, 'write', sandbox, iid);
+                assert.ok(isObject(base));
+                logObjectBaseAsWriteOperation(base, this.getSandbox(), iid);
             }
-            else if (f === Array.prototype.entries || f === Array.prototype.keys || f === Array.prototype.values
-                || f === Array.prototype.every || f === Array.prototype.find || f === Array.prototype.findIndex
-                || f === Array.prototype.indexOf || f === Array.prototype.lastIndexOf
-                || f === Array.prototype.forEach || f === Array.prototype.includes
-                || f === Array.prototype.join || f === Array.prototype.reduce || f === Array.prototype.reduceRight
-                || f === Array.prototype.some || f === Array.prototype.toString || f === Array.prototype.toLocaleString
+            else if (f === Array.prototype.every
+                || f === Array.prototype.find
+                || f === Array.prototype.findIndex
+                || f === Array.prototype.indexOf
+                || f === Array.prototype.lastIndexOf
+                || f === Array.prototype.forEach
+                || f === Array.prototype.includes
+                || f === Array.prototype.join
+                || f === Array.prototype.reduce
+                || f === Array.prototype.reduceRight
+                || f === Array.prototype.some
+                || f === Array.prototype.toString
+                || f === Array.prototype.toLocaleString
             )
             {
-                assert.ok(Array.isArray(base));
-                ObjectLogStore.appendObjectOperation(base, 'read', sandbox, iid);
-            }
-            else if (f === Array.prototype.fill)
-            {
-                if (Array.isArray(base))
-                {
-                    ObjectLogStore.appendObjectOperation(base, 'write', sandbox, iid);
-                }
-            }
-            else if (f === Array.prototype.pop || f === Array.prototype.push || f === Array.prototype.shift
-                || f === Array.prototype.reverse || f === Array.prototype.sort || f === Array.prototype.unshift)
-            {
-                assert.ok(Array.isArray(base));
-                ObjectLogStore.appendObjectOperation(base, 'write', sandbox, iid);
+                assert.ok(isObject(base));
+                logObjectBaseAsReadOperation(base, this.getSandbox(), iid);
             }
             else if (f === Array.prototype.filter
                 || f === Array.prototype.flat
@@ -85,19 +76,15 @@ export class ArrayOperationLogger extends Analysis
                 || f === Array.prototype.map
                 || f === Array.prototype.slice)
             {
-                assert.ok(Array.isArray(base));
-                ObjectLogStore.appendObjectOperation(base, 'read', sandbox, iid);
-
-                assert.ok(Array.isArray(result));
-                ObjectLogStore.appendObjectOperation(result, 'write', sandbox, iid);
+                assert.ok(isObject(base));
+                logObjectBaseAsReadOperation(base, this.getSandbox(), iid);
+                logObjectResultAsWriteOperation(result, this.getSandbox(), iid);
             }
             else if (f === Array.prototype.splice)
             {
-                assert.ok(Array.isArray(base));
-                ObjectLogStore.appendObjectOperation(base, 'write', sandbox, iid);
-
-                assert.ok(Array.isArray(result));
-                ObjectLogStore.appendObjectOperation(result, 'write', sandbox, iid);
+                assert.ok(isObject(base));
+                logObjectBaseAsWriteOperation(base, this.getSandbox(), iid);
+                logObjectResultAsWriteOperation(result, this.getSandbox(), iid);
             }
         };
     }
