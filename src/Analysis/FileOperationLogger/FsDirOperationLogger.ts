6ac422d03cb1058533f98dd517049952e5a65b1f
Soulike <soulike@qq.com>
2021-08-24 16:54:11 +0800

    添加了 fs.Dir 建模

---
+++ src/Analysis/FileOperationLogger/FsDirOperationLogger.ts
@@ -0,0 +1,33 @@
+// DO NOT INSTRUMENT
+
+import {Dir} from 'fs';
+import {IteratorLogStore} from '../../LogStore/IteratorLogStore';
+import {Analysis, Hooks, Sandbox} from '../../Type/nodeprof';
+import {FileOperationLogger} from './FileOperationLogger';
+
+export class FsDirOperationLogger extends Analysis
+{
+    public invokeFun: Hooks['invokeFun'] | undefined;
+    
+    constructor(sandbox: Sandbox)
+    {
+        super(sandbox);
+
+        this.registerHooks();
+    }
+
+    protected override registerHooks()
+    {
+        this.invokeFun = (iid, f, base) =>
+        {
+            if (f === Dir.prototype.read
+            || f === Dir.prototype.readSync)
+            {
+                if(base instanceof Dir)
+                {
+                    FileOperationLogger.appendOperation(base.path, 'read', this.getSandbox(), iid);
+                }
+            }
+        }
+    }
+}
