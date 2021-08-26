4c48c53c0ab07e287b82f223a93b33f55d5a2093
Soulike <soulike@qq.com>
2021-08-26 18:19:03 +0800

    初步实现保守检测算法

--- src/RaceDetector/ConservativeDetector.ts
+++ src/RaceDetector/ConservativeDetector.ts
@@ -1,8 +1,73 @@
 // DO NOT INSTRUMENT
 
+import {CallbackFunction} from '../LogStore/Class/CallbackFunction';
 import {Detector} from './Detector';
+import {ViolationInfo} from './ViolationInfo';
 
-export const conservativeDetector: Detector = () =>
+const checkedCallbacks: WeakSet<CallbackFunction> = new WeakSet();
+
+export const conservativeDetector: Detector = (resourceDeclaration) =>
 {
-    return null;
-}
+    const callbackToOperations = resourceDeclaration.getCallbackFunctionToOperations();
+    const LENGTH = callbackToOperations.size;
+    if (LENGTH <= 2)
+    {
+        return null;
+    }
+
+    const callbackToOperationsArray = Array.from(callbackToOperations.entries());
+
+    const lastCallbackToOperation = callbackToOperationsArray[LENGTH - 1]!;
+    const lastCallback = lastCallbackToOperation[0];
+
+    if (checkedCallbacks.has(lastCallback))
+    {
+        
+        return null;
+    }
+    else
+    {
+        checkedCallbacks.add(lastCallback);
+    }
+
+    const lastCallbackAsyncIds: Set<number> = new Set();
+    let currentCallback = lastCallback.asyncScope;
+    while (currentCallback !== null)
+    {
+        lastCallbackAsyncIds.add(currentCallback.asyncId);
+        currentCallback = currentCallback.asyncScope;
+    }
+
+    let atomicPairIndex1 = -1;
+    let atomicPairIndex2 = LENGTH - 1;
+    let violatingOperationIndex = -1;
+
+    for (let i = callbackToOperationsArray.length - 2; i >= 0; i--)
+    {
+        if (lastCallbackAsyncIds.has(callbackToOperationsArray[i]![0].asyncId))
+        {
+            atomicPairIndex1 = i;
+            break;
+        }
+    }
+    if (atomicPairIndex1 === -1)
+    {
+        return null;
+    }
+
+    for (let i = atomicPairIndex1 + 1; i < atomicPairIndex2; i++)
+    {
+        const operations = callbackToOperationsArray[i]![1];
+        if (!operations.every(operation => operation.getType() !== 'write'))
+        {
+            violatingOperationIndex = i;
+            break;
+        }
+    }
+    if (violatingOperationIndex === -1)
+    {
+        return null;
+    }
+
+    return new ViolationInfo(resourceDeclaration, [atomicPairIndex1, atomicPairIndex2], violatingOperationIndex);
+};
--- src/RaceDetector/EventEmitter.ts
+++ src/RaceDetector/EventEmitter.ts
@@ -2,6 +2,7 @@
 
 import EventEmitter from 'events';
 import {ResourceDeclaration} from '../LogStore/Class/ResourceDeclaration';
+import {toJSON} from '../Util';
 import {aggressiveDetector} from './AggressiveDetector';
 import {conservativeDetector} from './ConservativeDetector';
 
@@ -29,7 +30,29 @@ export const eventEmitter = new RaceDetectorEventEmitter();
 
 eventEmitter.on('operationAppended', (resourceDeclaration) =>
 {
-    conservativeDetector(resourceDeclaration);
+    const violationInfo = conservativeDetector(resourceDeclaration);
+    if (violationInfo !== null)
+    {
+        const {
+            resourceDeclaration,
+            atomicOperationsPairIndexes,
+            violatingOperationIndex} = violationInfo;
+        const callbackFunctionToOperationsArray = Array.from(resourceDeclaration.getCallbackFunctionToOperations());
+        const modifiedResourceDeclaration: any = {...resourceDeclaration};
+        delete modifiedResourceDeclaration.callbackFunctionToOperations;
+        delete modifiedResourceDeclaration.operations;
+        
+        console.log(
+`on
+${toJSON(modifiedResourceDeclaration)}
+found violation
+${toJSON(callbackFunctionToOperationsArray[atomicOperationsPairIndexes[0]])}
+and
+${toJSON(callbackFunctionToOperationsArray[atomicOperationsPairIndexes[1]])}
+are violated by
+${toJSON(callbackFunctionToOperationsArray[violatingOperationIndex])}
+`);
+    }
 });
 eventEmitter.on('operationAppended', (resourceDeclaration) =>
 {
