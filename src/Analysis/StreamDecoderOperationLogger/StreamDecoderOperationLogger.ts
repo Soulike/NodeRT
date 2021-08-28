1a8e9f2c8d87ae35167598cffbf39dd28069cc5b
Soulike <soulike@qq.com>
2021-08-28 17:13:40 +0800

    添加了对 StringDecoder 模块的建模

---
+++ src/Analysis/StreamDecoderOperationLogger/StreamDecoderOperationLogger.ts
@@ -0,0 +1,40 @@
+// DO NOT INSTRUMENT
+
+import {StringDecoder} from 'string_decoder';
+import {BufferLogStore} from '../../LogStore/BufferLogStore';
+import {Analysis, Hooks, Sandbox} from '../../Type/nodeprof';
+import {getSourceCodeInfoFromIid, isBufferLike} from '../../Util';
+
+export class StringDecoderOperationLogger extends Analysis
+{
+    public invokeFun: Hooks['invokeFun'] | undefined;
+
+    constructor(sandbox: Sandbox)
+    {
+        super(sandbox);
+
+        this.registerHooks();
+    }
+
+    protected override registerHooks()
+    {
+        this.invokeFun = (iid, f, _base, args) =>
+        {
+            if (f === StringDecoder.prototype.write)
+            {
+                const [buffer] = args as Parameters<typeof StringDecoder.prototype.write>;
+                BufferLogStore.appendBufferOperation(buffer, 'read',
+                    getSourceCodeInfoFromIid(iid, this.getSandbox()));
+            }
+            else if (f === StringDecoder.prototype.end)
+            {
+                const [buffer] = args as Parameters<typeof StringDecoder.prototype.end>;
+                if (isBufferLike(buffer))
+                {
+                    BufferLogStore.appendBufferOperation(buffer, 'read',
+                        getSourceCodeInfoFromIid(iid, this.getSandbox()));
+                }
+            }
+        };
+    }
+}
