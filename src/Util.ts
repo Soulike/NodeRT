63d02ed6ac89f9213898b77fd9f458d0bf4fe197
Soulike <soulike@qq.com>
2021-09-01 15:42:28 +0800

    删除冗余代码

--- src/Util.ts
+++ src/Util.ts
@@ -1,17 +1,10 @@
 // DO NOT INSTRUMENT
 
-import fs from 'fs';
 import {Sandbox} from './Type/nodeprof';
 import {SourceCodeInfo} from './LogStore/Class/SourceCodeInfo';
 import {Range} from './LogStore/Class/Range';
 import {BufferLike} from './Analysis/Type/BufferLike';
 import util from 'util';
-import {isFunction} from 'lodash';
-
-export function printSync(content: string): void
-{
-    fs.writeFileSync(1, `${content}\n`, {encoding: 'utf-8'});
-}
 
 export function toJSON(object: unknown): string
 {
@@ -91,24 +84,4 @@ export function isArrayAccess(isComputed: boolean, offset: string | Symbol): boo
 export function isURL(other: unknown): other is URL
 {
     return other instanceof URL;
-}
-
-export function getFunctionProperties(object: any): Set<Function> | never
-{
-    const functions: Set<Function> = new Set();
-    Object.getOwnPropertyNames(object).forEach(key =>
-    {
-        try
-        {
-            if (isFunction(object[key]))
-            {
-                functions.add(object[key]);
-            }
-        }
-        catch (e)
-        {
-            // ignore
-        }
-    });
-    return functions;
 }
