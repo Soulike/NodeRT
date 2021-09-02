179af8982ac4d2e70518671138a58e94c7387a57
Soulike <soulike@qq.com>
2021-09-02 16:08:15 +0800

    优化 nodeprof 启动方式，添加计时

--- src/bin/nodeprof.ts
+++ src/bin/nodeprof.ts
@@ -3,13 +3,20 @@
 import childProcess from 'child_process';
 import path from 'path';
 
+console.time('analysis');
+
+process.on('exit', () =>
+{
+    console.timeEnd('analysis');
+});
+
 const ROOT = path.resolve(__dirname, '..', '..');
 
 const {error} = childProcess.spawnSync(`graalnode`, [
     '--jvm',
     '--experimental-options',
     `--vm.Dtruffle.class.path.append=${path.resolve(ROOT, './lib/nodeprof.jar')}`,
-    '--nodeprof.Scope=module',
+    '--nodeprof.Scope=app',
     '--nodeprof.ExcludeSource=test',
     '--nodeprof', path.resolve(ROOT, './lib/nodeprof.js/src/ch.usi.inf.nodeprof/js/jalangi.js'),
     ...process.argv.slice(2),
