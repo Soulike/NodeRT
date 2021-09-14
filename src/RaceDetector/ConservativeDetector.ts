0a400ef5e660c59639bc659f27f8ed5a7facc245
Soulike <soulike@qq.com>
2021-09-14 17:37:29 +0800

    优化保守检测算法

--- src/RaceDetector/ConservativeDetector.ts
+++ src/RaceDetector/ConservativeDetector.ts
@@ -1,17 +1,21 @@
 // DO NOT INSTRUMENT
 
-import {CallbackFunction} from '../LogStore/Class/CallbackFunction';
 import {Detector} from './Detector';
 import {shouldOutput} from './Util';
 import {ViolationInfo} from './ViolationInfo';
+import {ResourceDeclaration} from '../LogStore/Class/ResourceDeclaration';
 
-const checkedCallbacks: WeakSet<CallbackFunction> = new WeakSet();
+/**
+ * For curtain resourceDeclaration, which asyncIds have been reported forming violations
+ * We do not need repeated violation reports for the same asyncId with multiple operations
+ * */
+const resourceDeclarationToProcessedAsyncIds = new WeakMap<ResourceDeclaration, Set<number>>();
 
 export const conservativeDetector: Detector = (resourceDeclaration) =>
 {
     const callbackToOperations = resourceDeclaration.getCallbackFunctionToOperations();
     const LENGTH = callbackToOperations.size;
-    if (LENGTH <= 2)
+    if (LENGTH <= 2)    // no conflict if there are only 2 operations
     {
         return null;
     }
@@ -21,14 +25,23 @@ export const conservativeDetector: Detector = (resourceDeclaration) =>
     const lastCallbackToOperation = callbackToOperationsArray[LENGTH - 1]!;
     const lastCallback = lastCallbackToOperation[0];
 
-    if (checkedCallbacks.has(lastCallback))
+    // check if the callback has been processed for the resourceDeclaration
+    const processedAsyncIds = resourceDeclarationToProcessedAsyncIds.get(resourceDeclaration);
+    if (processedAsyncIds === undefined)
+    {
+        resourceDeclarationToProcessedAsyncIds.set(resourceDeclaration, new Set([lastCallback.asyncId]));
+    }
+    else if (processedAsyncIds.has(lastCallback.asyncId))
     {
         return null;
     }
 
+    /**
+     * asyncId chain for the last callback
+     * */
     const lastCallbackAsyncIds: Set<number> = new Set();
     let currentCallback = lastCallback.asyncScope;
-    while (currentCallback !== null)
+    while (currentCallback !== null)    // get the async id chain
     {
         lastCallbackAsyncIds.add(currentCallback.asyncId);
         currentCallback = currentCallback.asyncScope;
@@ -38,9 +51,10 @@ export const conservativeDetector: Detector = (resourceDeclaration) =>
     let atomicPairIndex2 = LENGTH - 1;
     let violatingOperationIndex = -1;
 
+    // From the last to the first, check if another callback can form atomic pair with the last callback
     for (let i = callbackToOperationsArray.length - 2; i >= 0; i--)
     {
-        if (lastCallbackAsyncIds.has(callbackToOperationsArray[i]![0].asyncId))
+        if (lastCallbackAsyncIds.has(callbackToOperationsArray[i]![0].asyncId)) // on the chain
         {
             atomicPairIndex1 = i;
             break;
@@ -51,12 +65,13 @@ export const conservativeDetector: Detector = (resourceDeclaration) =>
         return null;
     }
 
+    // check whether there is another callback between the atomic pair above writes to the resource
     for (let i = atomicPairIndex1 + 1; i < atomicPairIndex2; i++)
     {
         const operations = callbackToOperationsArray[i]![1];
         const callback = callbackToOperationsArray[i]![0];
         if (!operations.every(operation => operation.getType() !== 'write')
-            && callback.asyncId !== lastCallback.asyncId)   // for setInterval callbacks
+            && callback.asyncId !== lastCallback.asyncId)   // for setInterval callbacks, which have the same asyncId, and do not violate each other
         {
             violatingOperationIndex = i;
             break;
@@ -71,7 +86,9 @@ export const conservativeDetector: Detector = (resourceDeclaration) =>
 
     if (shouldOutput(violationInfo, callbackToOperationsArray))
     {
-        checkedCallbacks.add(callbackToOperationsArray[violatingOperationIndex]![0]);
+        resourceDeclarationToProcessedAsyncIds.set(resourceDeclaration,
+            (resourceDeclarationToProcessedAsyncIds.get(resourceDeclaration) ?? new Set<number>())
+                .add(lastCallback.asyncId));
         return violationInfo;
     }
     else
