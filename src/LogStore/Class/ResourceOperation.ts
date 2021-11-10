37be82b861280dd0faf4df86d257187bf55dcb5c
Soulike <soulike@qq.com>
2021-11-10 17:29:13 +0800

    ResourceOperation 添加时间戳记录

--- src/LogStore/Class/ResourceOperation.ts
+++ src/LogStore/Class/ResourceOperation.ts
@@ -7,12 +7,14 @@ export abstract class ResourceOperation
     private readonly type: 'read' | 'write';
     private readonly scopeCodeInfo: SourceCodeInfo;
     private readonly stackTrace: string[] | null;
+    private readonly timestamp: number;
 
     protected constructor(type: 'read' | 'write', stackTrace: string[] | null, sourceCodeScopeInfo: SourceCodeInfo)
     {
         this.type = type;
         this.stackTrace = stackTrace;
         this.scopeCodeInfo = sourceCodeScopeInfo;
+        this.timestamp = Date.now();
     }
 
     public getType()
@@ -28,5 +30,10 @@ export abstract class ResourceOperation
     public getStackTrace()
     {
         return this.stackTrace;
+    }
+
+    public getTimestamp()
+    {
+        return this.timestamp;
     }
 }
