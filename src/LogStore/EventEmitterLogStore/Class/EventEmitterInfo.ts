18be68e2a8b9ba63be4a0e8d6c7a963651b7f631
Soulike <soulike@qq.com>
2021-11-26 11:24:06 +0800

    EventEmitterInfo 修改为弱引用

--- src/LogStore/EventEmitterLogStore/Class/EventEmitterInfo.ts
+++ src/LogStore/EventEmitterLogStore/Class/EventEmitterInfo.ts
@@ -4,19 +4,19 @@ import {SourceCodeInfo} from '../../Class/SourceCodeInfo';
 
 export class EventEmitterInfo extends ResourceInfo
 {
-    private readonly eventEmitter: EventEmitter;
+    private readonly eventEmitter: WeakRef<EventEmitter>;
     private readonly event: string | symbol;
 
     constructor(eventEmitter: EventEmitter, event: string | symbol, possibleDefineCodeScope: SourceCodeInfo | null)
     {
         super('eventEmitter', possibleDefineCodeScope);
-        this.eventEmitter = eventEmitter;
+        this.eventEmitter = new WeakRef(eventEmitter);
         this.event = event;
     }
 
     public is(other: unknown, event?: string | symbol): boolean
     {
-        return this.eventEmitter === other && event === this.event;
+        return this.eventEmitter.deref() === other && event === this.event;
     }
 
     public toJSON()
