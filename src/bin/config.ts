99df970680e239fb8bcff5e5bce9bfd697bbdaf2
Soulike <soulike@qq.com>
2021-11-26 10:56:02 +0800

    暂时使用 graaljs 21.2.0

--- src/bin/config.ts
+++ src/bin/config.ts
@@ -1,4 +1,4 @@
 import path from 'path';
 
 export const ROOT = path.resolve(__dirname, '..', '..');
-export const NODEPROF_PATH = path.resolve(ROOT, './nodeprof.js/nodeprof.21.3.0.jar');
+export const NODEPROF_PATH = path.resolve(ROOT, './nodeprof.js/nodeprof.21.2.0.jar');
